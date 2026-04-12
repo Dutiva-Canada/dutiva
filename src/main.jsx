5168173767725788aaebcccf6021130588fce463
Martin Constantineau <266895517+martinconstantineau@users.noreply.github.com>
2026-04-12 15:38:24 -0400

    seo: wrap app with HelmetProvider in main.jsx
    Signed-off-by: Martin Constantineau <266895517+martinconstantineau@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
@@ -8,12 +9,14 @@ import AppErrorBoundary from './components/AppErrorBoundary.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppErrorBoundary>
-      <AuthProvider>
-        <PlanProvider>
-          <App />
-        </PlanProvider>
-      </AuthProvider>
-    </AppErrorBoundary>
+    <HelmetProvider>
+      <AppErrorBoundary>
+        <AuthProvider>
+          <PlanProvider>
+            <App />
+          </PlanProvider>
+        </AuthProvider>
+      </AppErrorBoundary>
+    </HelmetProvider>
   </StrictMode>,
 );
